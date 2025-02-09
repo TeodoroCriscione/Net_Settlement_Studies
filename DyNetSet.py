6eefce477e57b787653902a1f26bf002ec9eb223
Teodoro Criscione <teodoro.criscione@gmail.com>
2025-02-09 22:18:08 +0100

    Dijkstra now works with negative weight, in case of concurrent cycles the largest is cancelled. Example of concurrent cycles added.

--- DyNetSet.py
+++ DyNetSet.py
@@ -36,9 +36,31 @@ def update_graph(G, bundle):
             G.remove_edges_from(path_edges_with_data)
             # find minimum weight
             path_edges_with_data.append(candidate)
-            path_edges_with_data = sorted(path_edges_with_data, key= lambda x: x[2]['weight'])
+            path_edges_with_data = sorted(path_edges_with_data, key= lambda x: x[2]['weight'],reverse=True)
             min_weight = path_edges_with_data[0][2]['weight']
             path_edges_to_add = [(s,t,{'weight':w['weight']-min_weight}) for s,t,w in path_edges_with_data[1:]]
+            path_edges_to_add = [e for e in path_edges_to_add if e[2]['weight'] != 0]
             # update graph 
-            G.add_edges_from(bundle)
+            G.add_edges_from(path_edges_to_add)
     return G
+
+# to maximize the flow cycle cancelling using the Dijkstra algorithm
+# the weights need to be negative
+def negGraph (G, bundle):
+    # Create a copy of the graph with negative edge weights
+    G_neg = G.copy()
+    for u, v, data in G_neg.edges(data=True):
+        data['weight'] = -data['weight']  # Negate the weight
+    # Create a copy of the bundle with negative weights
+    bundle = [(s,t,{'weight':(-1)*w['weight']}) for s,t,w in bundle]
+    # return
+    return G_neg, bundle
+
+# in case of concurrent cycles, choose the one with largest weights
+def cycle_cancelling(G, bundle):
+    G_neg, bundle = negGraph (G, bundle)
+    G_neg = update_graph(G_neg, bundle)
+    G = G_neg.copy()
+    for u, v, data in G.edges(data=True):
+        data['weight'] = (-1)*data['weight']  # positive weight
+    return G 
